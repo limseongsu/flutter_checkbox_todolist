60e6d2f124ac457d304fb6288fe4082d2e6b5ebd
imseongsu <tjdtn2323@naver.com>
2021-05-07 11:53:51 +0900

    checkbox를 이용하여 todolist만들기

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:grouped_buttons/grouped_buttons.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ToDoListPage(),
+    );
+  }
+}
+
+class ToDoListPage extends StatefulWidget {
+  const ToDoListPage({Key key}) : super(key: key);
+
+  @override
+  _ToDoListPageState createState() => _ToDoListPageState();
+}
+
+class _ToDoListPageState extends State<ToDoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDoList'),
+      ),
+      body: ListView(
+        children: [
+          Container(),
+        ],
+      ),
+    );;
+  }
+}
+
+
+
+
